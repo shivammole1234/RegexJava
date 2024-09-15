7a243a51ad0a6a9d820680d6d245ca771c3743bc
Shivam Mole <shivammole1713@gmail.com>
2024-09-15 20:39:39 +0530

    commiting on regex postal code uc1

--- src/main/java/regex/bridgelabz/Javaregex.java
+++ src/main/java/regex/bridgelabz/Javaregex.java
@@ -1,4 +1,26 @@
 package regex.bridgelabz;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Javaregex {
+
+    public static boolean check_regex(String regex ,String str){
+
+        Pattern pattern = Pattern.compile(regex);
+
+        Matcher matcher=pattern.matcher(str);
+        return matcher.matches();
+    }
+    public static void main(String[] args) {
+
+        String str="652255";
+        String regex="^[1-9][0-9]{5}$\n";
+
+        if(check_regex(regex,str))
+            System.out.println("the string :- "+str+" match with the regex:-  "+regex);
+        else
+            System.out.println("the string :- "+str+" match with the regex:-  "+regex);
+
+
+    }
 }
