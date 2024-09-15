2d81ab5d86f5d0df1cfcf074331a3b1d42c8b0a9
Shivam Mole <shivammole1713@gmail.com>
2024-09-15 20:53:48 +0530

    commititng regex code for the test case 4 i.e 400088 & 400 088

--- src/main/java/regex/bridgelabz/Javaregex.java
+++ src/main/java/regex/bridgelabz/Javaregex.java
@@ -14,7 +14,7 @@ public class Javaregex {
     public static void main(String[] args) {
 
         String str="802255";
-        String regex="^[1-9][0-9]{5}$\n";
+        String regex="^[1-9][0-9]{2}\\s?[0-9]{3}$";
 
         if(check_regex(regex,str))
             System.out.println("the string :- "+str+" match with the regex:-  "+regex);
